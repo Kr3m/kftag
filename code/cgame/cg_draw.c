daa18f3ce45f1ef95bc6b35ab9c6d4b39f475752
Kevin Remisoski <kremisoski@gmail.com>
2023-04-27 23:40:14 -0400

    spawn protection warning working

--- code/cgame/cg_draw.c
+++ code/cgame/cg_draw.c
@@ -2122,6 +2122,7 @@ CG_DrawCrosshairNames
 static void CG_DrawCrosshairNames( void ) {
 	float		*color;
 	const char	*name;
+	vec4_t		hcolor;
 
 	if ( !cg_drawCrosshair.integer ) {
 		return;
@@ -2136,15 +2137,6 @@ static void CG_DrawCrosshairNames( void ) {
 	// scan the known entities to see if the crosshair is sighted on one
 	CG_ScanForCrosshairEntity();
 
-	// draw spawn protection
-	if ( cg.snap->ps.eFlags & EF_SPAWNPROTECTION ) {
-		color[0] = 1;
-		color[1] = 0;
-		color[2] = 0;
-		color[3] = 1;
-		CG_DrawString( 320, 72, "SPAWN PROTECTION", color, BIGCHAR_WIDTH, BIGCHAR_HEIGHT, 0, DS_CENTER | DS_PROPORTIONAL );
-	}
-
 	// draw the name of the player being looked at
 	color = CG_FadeColor( cg.crosshairClientTime, 1000 );
 	if ( !color ) {
@@ -2157,21 +2149,18 @@ static void CG_DrawCrosshairNames( void ) {
 	color[3] *= 0.5f;
 	w = CG_Text_Width(name, 0.3f, 0);
 	CG_Text_Paint( 320 - w / 2, 190, 0.3f, color, name, 0, 0, ITEM_TEXTSTYLE_SHADOWED);
-#else
+#else	
+	if ( cg_entities[cg.crosshairClientNum].currentState.eFlags & EF_SPAWNPROTECTION ) {
+		hcolor[0] = 1;
+		hcolor[1] = 0;
+		hcolor[2] = 0;
+		hcolor[3] = 1;
+		CG_DrawString( 320, 72, "SPAWN PROTECTION", hcolor, BIGCHAR_WIDTH, BIGCHAR_HEIGHT, 0, DS_CENTER | DS_PROPORTIONAL );
+	}
 	color[3] *= 0.5f;
 	CG_DrawString( 320, 174, name, color, BIGCHAR_WIDTH, BIGCHAR_HEIGHT, 0, DS_CENTER | DS_PROPORTIONAL );
 #endif
 	trap_R_SetColor( NULL );
-}
-
-/*
-=====================
-CG_DrawSpawnProtection
-=====================
-*/
-
-static void CG_DrawSpawnProtection( void ) {
-	
 }
 
 
