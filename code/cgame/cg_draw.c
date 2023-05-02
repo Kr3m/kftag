d3a0083751813d482bf222f3faeff93cc755cfff
Kevin Remisoski <kremisoski@gmail.com>
2023-05-02 08:19:47 -0400

    testing initial changes

--- code/cgame/cg_draw.c
+++ code/cgame/cg_draw.c
@@ -1036,7 +1036,13 @@ static float CG_DrawTeamOverlay( float y, qboolean right, qboolean upper ) {
 
 			CG_GetColorForHealth( ci->health, ci->armor, hcolor );
 
-			Com_sprintf (st, sizeof(st), "%3i %3i", ci->health,	ci->armor);
+			//Com_sprintf (st, sizeof(st), "%3i %3i", ci->health,	ci->armor);
+			if (ci->health == 0) { // frozen so armor is countdown
+				Com_sprintf(st, sizeof(st), "  %3i  ", ci->armor);
+			} else {
+				CG_GetColorForHealth( ci->health, ci->armor, hcolor );
+				Com_sprintf (st, sizeof(st), "%3i %3i", ci->health,	ci->armor);
+			}
 
 			xx = x + TINYCHAR_WIDTH * 3 + 
 				TINYCHAR_WIDTH * pwidth + TINYCHAR_WIDTH * lwidth;
