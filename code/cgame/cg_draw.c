03197445ead61949dc6b0bded0bff651fc6d0104
Kevin Remisoski <kremisoski@gmail.com>
2023-05-02 10:29:46 -0400

    working on team overlay

--- code/cgame/cg_draw.c
+++ code/cgame/cg_draw.c
@@ -1038,6 +1038,7 @@ static float CG_DrawTeamOverlay( float y, qboolean right, qboolean upper ) {
 
 			//Com_sprintf (st, sizeof(st), "%3i %3i", ci->health,	ci->armor);
 			if (ci->health == 0) { // frozen so armor is countdown
+				//CG_GetColorForHealth( ci->health, ci->armor, hcolor );
 				Com_sprintf(st, sizeof(st), "  %3i  ", ci->armor);
 			} else {
 				CG_GetColorForHealth( ci->health, ci->armor, hcolor );
