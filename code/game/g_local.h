b83068a4ce469fd41da420e549d4c7e818de813f
Kevin Remisoski <kremisoski@gmail.com>
2023-05-05 02:47:33 -0400

    changed version number

--- code/game/g_local.h
+++ code/game/g_local.h
@@ -10,7 +10,7 @@
 
 // the "gameversion" client command will print this plus compile date
 //#define	GAMEVERSION	"baseq3"
-#define	GAMEVERSION	"kftag-beta003"
+#define	GAMEVERSION	"kftag-beta004"
 
 #define BODY_QUEUE_SIZE		8
 
