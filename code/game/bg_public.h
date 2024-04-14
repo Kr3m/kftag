5456ff917ab54de8e015d9287cfe37aacea03130
Kr3m <kremisoski@gmail.com>
2024-04-13 21:12:17 -0400

    fixed fastrail and fastweaponswitch

--- code/game/bg_public.h
+++ code/game/bg_public.h
@@ -175,6 +175,9 @@ typedef struct {
 
 	int			grapplePull;
 
+	int			fastWeaponSwitch;
+	int			fastRail;
+
 	// callbacks to test the world
 	// these will be different functions during game and cgame
 	void		(*trace)( trace_t *results, const vec3_t start, const vec3_t mins, const vec3_t maxs, const vec3_t end, int passEntityNum, int contentMask );
