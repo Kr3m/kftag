ef9549d80f471a60eec2160e04ccfc60f9fd0509
Kr3m <kr3m@dayentech.net>
2025-05-03 03:19:58 -0400

    fixed BG_CleanName to get shadows working

--- code/game/bg_misc.c
+++ code/game/bg_misc.c
@@ -1967,87 +1967,87 @@ BG_CleanName
 ============
 */
 void BG_CleanName( const char *in, char *out, int outSize, const char *blankString ) {
-	int		len, colorlessLen;
-	char	ch;
-	char	*p;
-	int		spaces;
+    int        len, colorlessLen;
+    char    ch;
+    char    *p;
+    int        spaces;
 
-	//save room for trailing null byte
-	outSize--;
+    //save room for trailing null byte
+    outSize--;
 
-	len = 0;
-	colorlessLen = 0;
-	p = out;
-	*p = '\0';
-	spaces = 0;
+    len = 0;
+    colorlessLen = 0;
+    p = out;
+    *p = '\0';
+    spaces = 0;
 
-	while( 1 ) {
-		ch = *in++;
-		if( !ch ) {
-			break;
-		}
+    while( 1 ) {
+        ch = *in++;
+        if( !ch ) {
+            break;
+        }
 
-		// don't allow leading spaces
-		if( *p == '\0' && ch <= ' ' ) {
-			continue;
-		}
+        // don't allow leading spaces
+        if( *p == '\0' && ch <= ' ' ) {
+            continue;
+        }
 
-		// check colors
-		if( ch == Q_COLOR_ESCAPE ) {
-			// solo trailing carat is not a color prefix
-			if( !*in ) {
-				break;
-			}
+        // check colors
+        if( ch == Q_COLOR_ESCAPE ) {
+            // solo trailing carat is not a color prefix
+            if( !*in ) {
+                break;
+            }
 
-			// don't allow black in a name, period
-			if( ColorIndex(*in) == 0 ) {
-				in++;
-				continue;
-			}
+            // don't allow black in a name, period
+            //if( ColorIndex(*in) == 0 ) {
+            //    in++;
+            //    continue;
+            //}
 
-			// make sure room in dest for both chars
-			if( len > outSize - 2 ) {
-				break;
-			}
+            // make sure room in dest for both chars
+            if( len > outSize - 2 ) {
+                break;
+            }
 
-			*out++ = ch;
-			*out++ = *in++;
-			len += 2;
-			continue;
-		}
+            *out++ = ch;
+            *out++ = *in++;
+            len += 2;
+            continue;
+        }
 
-		// let's keep it in printable range
-		if ( ch < ' ' || ch > 126 ) {
-			continue;
-		}
+        // let's keep it in printable range
+        //if ( ch < ' ' || ch > 126 ) {
+        //    continue;
+        //}
 
-		// don't allow too many consecutive spaces
-		if( ch == ' ' ) {
-			spaces++;
-			if( spaces > 2 ) {
-				continue;
-			}
-		}
-		else {
-			spaces = 0;
-		}
+        // don't allow too many consecutive spaces
+        if( ch == ' ' ) {
+            spaces++;
+            if( spaces > 2 ) {
+                continue;
+            }
+        }
+        else {
+            spaces = 0;
+        }
 
-		if( len > outSize - 1 ) {
-			break;
-		}
+        if( len > outSize - 1 ) {
+            break;
+        }
 
-		*out++ = ch;
-		colorlessLen++;
-		len++;
-	}
-	*out = '\0';
+        *out++ = ch;
+        colorlessLen++;
+        len++;
+    }
+    *out = '\0';
 
-	if ( blankString ) {
-		// don't allow empty names
-		if( *p == '\0' || colorlessLen == 0 ) {
-			Q_strncpyz( p, blankString, outSize );
-		}
-	}
+    if ( blankString ) {
+        // don't allow empty names
+        if( *p == '\0' || colorlessLen == 0 ) {
+            Q_strncpyz( p, blankString, outSize );
+        }
+    }
 }
 
 
