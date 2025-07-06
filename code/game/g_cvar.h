125d064162b9f55851ac885f6c0b35bbdcd0348c
Kr3m <kr3m@dayentech.net>
2025-07-06 12:08:10 -0400

    removed fps cap logic

--- code/game/g_cvar.h
+++ code/game/g_cvar.h
@@ -187,7 +187,6 @@ G_CVAR( g_rotation, "g_rotation", "0", CVAR_ARCHIVE, 0, qfalse, qfalse )
 G_CVAR( g_redteam, "g_redteam", "Stroggs", CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_USERINFO, 0, qtrue, qtrue )
 G_CVAR( g_blueteam, "g_blueteam", "Pagans", CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_USERINFO, 0, qtrue, qtrue )
 
-G_CVAR( g_fpsCap, "g_fpsCap", "250", CVAR_SERVERINFO, 0, qtrue, qfalse )
 G_CVAR( g_hitBoxEnabled, "g_hitBoxEnabled", "0", CVAR_SERVERINFO, 0, qfalse, qfalse )
 
 G_CVAR( g_healthDown, "g_healthDown", "1", CVAR_ARCHIVE, 0, qfalse, qfalse )
